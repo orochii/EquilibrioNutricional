ddb0e67964b3a39e35665c482945c20a48ed0a39
Orlando Marin Blanco <arcboundcrusher@gmail.com>
2019-08-17 12:26:33 -0600

    Status update cycle is done. Includes death and obesity.

---
+++ Assets/Scripts/StatBar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatBar : MonoBehaviour {
+    public enum BarDirection { HORIZONTAL, VERTICAL }
+
+    [SerializeField] private Image bar;
+    [SerializeField] private BarDirection barDirection;
+    private Vector2 startingSize;
+    private Vector2 startingRect;
+    [Range(0,1)] [SerializeField] private float m_value = .5f;
+    
+    public float Value {
+        get {
+            return m_value;
+        }
+        set {
+            m_value = Mathf.Clamp01(value);
+            RefreshSize();
+        }
+    }
+
+    private void RefreshSize() {
+        Vector2 newSize = startingRect * m_value;
+        if (barDirection == BarDirection.HORIZONTAL) {
+            bar.rectTransform.sizeDelta = new Vector2(newSize.x, startingRect.y);
+        } else {
+            bar.rectTransform.sizeDelta = new Vector2(startingRect.x, newSize.y);
+        }
+    }
+
+    void Awake() {
+        float rx = bar.rectTransform.rect.width;
+        float ry = bar.rectTransform.rect.height;
+        startingSize = new Vector2(bar.rectTransform.sizeDelta.x, bar.rectTransform.sizeDelta.y);
+        startingRect = new Vector2(rx, ry);
+        RefreshSize();
+    }
+
+    private void FixedUpdate() {
+        RefreshSize();
+    }
+}
--- Assets/Scripts/StatsManager.cs
+++ Assets/Scripts/StatsManager.cs
@@ -2,17 +2,128 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StatsManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class StatsManager : MonoBehaviour {
+    [SerializeField] private StatBar hungerBar;
+    [SerializeField] private StatBar fatBar;
+    [SerializeField] private StatBar sugarBar;
+    [SerializeField] private StatBar vitaminBar;
+
+    [Header("Rangos mínimos-máximos para estados")]
+    [SerializeField] private Vector2 hungerMinMax;
+    [SerializeField] private Vector2 fatMinMax;
+    [SerializeField] private Vector2 sugarMinMax;
+    [SerializeField] private Vector2 vitaminMinMax;
+
+    [Header("Tazas de cambio de estados por segundo")]
+    [SerializeField] private float decreaseRatePerSecond = 0.05f;
+    [SerializeField] private float debuffMultiplier = 2f;
+    [SerializeField] private float obesityMultiplier = 0.5f;
+    [SerializeField] private float obesityCooldown = 10f;
+
+    private float obesityTimer = 0;
+    private bool dead;
+    private string deathCause = "¿Hackeaste el juego mal?";
+
+    void Start() {
+        // Randomize starting stats.
+        float perc = UnityEngine.Random.Range(0.25f, 0.75f);
+        hungerBar.Value = Mathf.Lerp(hungerMinMax.x, hungerMinMax.y, perc);
+        perc = UnityEngine.Random.Range(0.25f, 0.75f);
+        fatBar.Value = Mathf.Lerp(fatMinMax.x, fatMinMax.y, perc);
+        perc = UnityEngine.Random.Range(0.25f, 0.75f);
+        sugarBar.Value = Mathf.Lerp(sugarMinMax.x, sugarMinMax.y, perc);
+        perc = UnityEngine.Random.Range(0.25f, 0.75f);
+        vitaminBar.Value = Mathf.Lerp(vitaminMinMax.x, vitaminMinMax.y, perc);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        // Do nothing if you're already dead.
+        if (dead) return;
+        // Initialize all rates
+        float hungerRate = decreaseRatePerSecond;
+        float fatRate = decreaseRatePerSecond;
+        float sugarRate = decreaseRatePerSecond;
+        float vitaminRate = decreaseRatePerSecond;
+        /*
+         * Calculate hunger rate and obesity effects.
+         */
+        if (!HasObesity) {
+            if (hungerBar.Value > hungerMinMax.y) obesityTimer = Time.time + obesityCooldown;
+        } else {
+            hungerRate *= obesityMultiplier;
+            fatRate *= obesityMultiplier;
+            sugarRate *= obesityMultiplier;
+            vitaminRate *= obesityMultiplier;
+        }
+        // Efecto de estados en otros estados
+        if (sugarBar.Value < sugarMinMax.x) hungerRate *= debuffMultiplier; // Si el azucar está baja
+        if (vitaminBar.Value > vitaminMinMax.y) hungerRate *= debuffMultiplier; // Si las vitaminas están altas
+        if (fatBar.Value < fatMinMax.x) vitaminRate *= debuffMultiplier; // Si la grasa está baja
+        // Submit all changes
+        hungerBar.Value -= hungerRate * Time.deltaTime;
+        fatBar.Value -= fatRate * Time.deltaTime;
+        sugarBar.Value -= sugarRate * Time.deltaTime;
+        vitaminBar.Value -= vitaminRate * Time.deltaTime;
+        // Check if you're... uh... sleeping with the fish -_- zZZ
+        // Muerte por inanición
+        if (hungerBar.Value <= hungerMinMax.x) {
+            dead = true;
+            deathCause = "Inanición";
+        }
+        // Muerte por paro cardíaco
+        if (fatBar.Value > fatMinMax.y) {
+            dead = true;
+            deathCause = "Paro cardíaco";
+        }
+        // Muerte por coma diabético
+        if (sugarBar.Value > sugarMinMax.y) {
+            dead = true;
+            deathCause = "Coma diabético";
+        }
+        // Muerte por escorbuto
+        if (vitaminBar.Value <= vitaminMinMax.x) {
+            dead = true;
+            deathCause = "Escorbuto";
+        }
+        // Do something if dead
+        if (dead) {
+            Debug.Log("Causa: " + deathCause);
+        }
     }
+
+    // Accessors to values/states
+    public bool HasObesity { get { return obesityTimer > Time.time; } }
+    public float Hunger {
+        get {
+            return hungerBar.Value;
+        }
+        set {
+            hungerBar.Value = value;
+        }
+    }
+    public float Fat {
+        get {
+            return fatBar.Value;
+        }
+        set {
+            fatBar.Value = value;
+        }
+    }
+    public float Sugar {
+        get {
+            return sugarBar.Value;
+        }
+        set {
+            sugarBar.Value = value;
+        }
+    }
+    public float Vitamin {
+        get {
+            return vitaminBar.Value;
+        }
+        set {
+            vitaminBar.Value = value;
+        }
+    }
+    public bool Dead { get { return dead; } }
 }
