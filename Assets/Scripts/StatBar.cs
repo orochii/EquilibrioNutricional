21d1d41944b33b363f140b05a10294ce867c60cb
Orlando Marin Blanco <arcboundcrusher@gmail.com>
2019-08-18 01:28:48 -0600

    Changed appearance of bars.

--- Assets/Scripts/StatBar.cs
+++ Assets/Scripts/StatBar.cs
@@ -12,6 +12,7 @@ public class StatBar : MonoBehaviour {
     private Vector2 startingSize;
     private Vector2 startingRect;
     [Range(0,1)] [SerializeField] private float m_value = .5f;
+    [SerializeField] private bool useFill = false;
     
     public float Value {
         get {
@@ -24,6 +25,11 @@ public class StatBar : MonoBehaviour {
     }
 
     private void RefreshSize() {
+        // Using fill is easy, and kewt
+        if (useFill) {
+            bar.fillAmount = m_value;
+            return;
+        }
         Vector2 newSize = startingRect * m_value;
         if (barDirection == BarDirection.HORIZONTAL) {
             bar.rectTransform.sizeDelta = new Vector2(newSize.x, startingRect.y);
