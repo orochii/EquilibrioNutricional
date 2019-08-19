c55ef6faa8730ced087f2cb3684b7fcd6ceeca24
Orlando Marin Blanco <arcboundcrusher@gmail.com>
2019-08-18 19:07:27 -0600

    Added flavour text to game over. Fixed screen responsiveness (hopefully).

--- Assets/Scripts/GameOverMessage.cs
+++ Assets/Scripts/GameOverMessage.cs
@@ -8,14 +8,18 @@ using System.Globalization;
 public class GameOverMessage : MonoBehaviour {
     [SerializeField] private Text deathCauseText;
     [SerializeField] private Text scoreText;
+    [SerializeField] private Text favouriteText;
+    [SerializeField] private Text lastFoodText;
 
     private int finalScore;
     
 
-    public void CallLose(string reason, int score = 0) {
+    public void CallLose(string reason, int score, string favourite, string lastFood) {
         deathCauseText.text = reason;
         finalScore = score;
         scoreText.text = finalScore.ToString("000 000 000");
+        favouriteText.text = favourite;
+        lastFoodText.text = lastFood;
         gameObject.SetActive(true);
     }
 
